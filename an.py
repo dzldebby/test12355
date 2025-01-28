da259bd750ef115022018634b36509e63270b469
dzldebby <debby.lingzl@gmail.com>
2025-01-28 18:19:07 +0800

    Reset repository with clean files

--- an.py
+++
@@ -1,5 +0,0 @@
-# Print contents of the file
-with open('interest_rates.csv', 'r') as file:
-    content = file.read()
-    print("CSV Contents:")
-    print(content)
--- config.py
+++
@@ -1,2 +0,0 @@
-# Your Telegram Bot Token
-TOKEN = '7734708333:AAGwGiHpTTkk5JjtmpU9b7egp1paJUzrbjU'
